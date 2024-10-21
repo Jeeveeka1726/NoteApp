ca4f85a6d41adaa09261947a9d9a777a45227295
Jaswaa <141898693+Jaswaa@users.noreply.github.com>
2024-10-21 14:32:06 +0530

    Get axios instance util function

--- Frontend/notesapp/src/components/Input/PasswordInput.jsx
+++ Frontend/notesapp/src/components/Input/PasswordInput.jsx
@@ -8,7 +8,7 @@ const PasswordInput = ({ value, onChange, placeholder }) => {
     const toggleShowPassword = () => {
         setIsShowPassword(!isShowPassword);
     };
-
+ 
     return (
         <div className='flex items-center bg-transparent border-[1.5px] px-5 rounded mb-3'>
             <input
