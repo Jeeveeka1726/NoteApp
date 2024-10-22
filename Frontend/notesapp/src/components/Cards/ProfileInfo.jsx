f074982e113f9c9fdfe20b1d7c109bd6680e7187
JEEVEEKA <jeeveeka.ps@kalvium.community>
2024-10-22 13:29:14 +0530

    completed setting up Get user API

--- Frontend/notesapp/src/components/Cards/ProfileInfo.jsx
+++ Frontend/notesapp/src/components/Cards/ProfileInfo.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import { getInitials } from '../../utils/helper'
 
-function ProfileInfo({onLogout}) {
+function ProfileInfo({userInfo, onLogout}) {
   return (
+    userInfo && (
     <div className='flex items-center gap-3'>
       <div className='w-12 h-12 flex items-center justify-center rounded-full text-slate-950 font-medium bg-slate-100 '>
-      {getInitials("John William")}
+      {getInitials(userInfo.fullName)}
       </div>
       <div>
-        <p className='text-sm font-medium'>John William</p>
+        <p className='text-sm font-medium'>{userInfo.fullName}</p>
         <button className='text-sm text-slate-700 underline' onClick={onLogout}>Logout</button>
       </div>
     </div>
+    )
   )
 }
 
