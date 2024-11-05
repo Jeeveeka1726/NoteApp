e7ad0e4d72ebecfac609a616891bb5de247a4aa1
Jaswaa <141898693+Jaswaa@users.noreply.github.com>
2024-11-05 17:44:38 +0530

    All Notes API integration

--- Frontend/notesapp/src/Pages/Home/Home.jsx
+++ Frontend/notesapp/src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import NoteCard from '../../Cards/NoteCard'
+// import moment from "moment"
 import {MdAdd} from "react-icons/md";
 import AddEditNotes from './AddEditNotes';
 import Model from "react-modal";
@@ -14,7 +15,8 @@ const Home = () => {
     type: null,
     date: null,
   });
-  
+
+  const [allNotes, setAllNotes] = useState([])
   const [userInfo, setUserInfo] = useState(null);
   const navigate = useNavigate();
 
@@ -30,9 +32,23 @@ const Home = () => {
         navigate("/login");
       }
     }
-  }
+  };
+
+  //Get all notes
+  const getAllNotes = async () => {
+    try{
+      const response = await axiosInstance.get("/get-all-notes");
+
+      if (response.data && response.data.notes){
+        setAllNotes(response.data.notes);
+      }
+    } catch (error) {
+      console.log("An unexpected error occurred. Please try again.");
+    }
+  };
 
   useEffect(()=>{
+    getAllNotes()
     getUserInfo();
     return()=>{}
   },[])
@@ -42,16 +58,19 @@ const Home = () => {
    <Navbar userInfo={userInfo}/>
     <div className="container mx-auto">
     <div className="grid grid-cols-3 gap-4 mt-8">
-     
-     <NoteCard 
-     title="Meeting on 7th April"
-     date="3rd Apr 2024"
-     content="Meeting on 7th April Meeting on 7th April" 
-     tags="#Meeting"
-     ispinned={true}
-     onEdit={()=>{}}
-     onDelete={()=>{}}
-     onPinNote={()=>{}}/>
+     {allNotes.map((item, index)=>(
+       <NoteCard 
+       key={item._id}
+       title={item.title}
+       date={item.createdOn}
+       content={item.content} 
+       tags={item.tags}
+       ispinned={item.isPinned}
+       onEdit={()=>{}}
+       onDelete={()=>{}}
+       onPinNote={()=>{}}/>
+     ))}
+    
    </div>
     </div>
 
